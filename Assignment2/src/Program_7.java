e081b41d95f47fa0a6d5d24a8d96e2e2431fa8d9
Udbhav227 <143270660+Udbhav227@users.noreply.github.com>
2023-11-20 15:24:04 +0530

    Update Program_7.java

--- Assignment2/src/Program_7.java
+++ Assignment2/src/Program_7.java
@@ -2,19 +2,10 @@ public class Program_7 {
     public static void main(String[] args) {
         int a = 1, b = 2;
         System.out.println("a\t b\t pow(a, b)");
-        
-        System.out.println(a + "\t " + b + "\t " + (int) Math.pow(a, b));
-
-        a = a + 1; b = b + 1;
-        System.out.println(a + "\t " + b + "\t " + (int) Math.pow(a, b));
-
-        a = a + 1; b = b + 1;
-        System.out.println(a + "\t " + b + "\t " + (int) Math.pow(a, b));
-
-        a = a + 1; b = b + 1;
-        System.out.println(a + "\t " + b + "\t " + (int) Math.pow(a, b));
-
-        a = a + 1; b = b + 1;
+        System.out.println(a + "\t " + b + "\t " + (int) Math.pow(a++, b++));
+        System.out.println(a + "\t " + b + "\t " + (int) Math.pow(a++, b++));
+        System.out.println(a + "\t " + b + "\t " + (int) Math.pow(a++, b++));
+        System.out.println(a + "\t " + b + "\t " + (int) Math.pow(a++, b++));
         System.out.println(a + "\t " + b + "\t " + (int) Math.pow(a, b));
     }
 }
