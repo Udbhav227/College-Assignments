e09184a556dcbf91db17428793940f5772bbe3af
Udbhav227 <143270660+Udbhav227@users.noreply.github.com>
2023-11-30 03:16:27 +0530

    Update Program_3.java

--- Assignment2/src/Using CLA/Program_3.java
+++ Assignment2/src/Using CLA/Program_3.java
@@ -1,7 +1,5 @@
 public class Program_3 {
     public static void main(String[] args) {
-        int res1 = (int)(6 * Math.random()) + 1;
-        int res2 = (int)(6 * Math.random()) + 1;
-        System.out.println(res1 + res2);
+        // soon
     }
 }
