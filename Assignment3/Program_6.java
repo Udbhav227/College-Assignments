2472379753f1ff6e6a19108460b501d4a24a8046
Udbhav227 <143270660+Udbhav227@users.noreply.github.com>
2023-11-30 17:57:07 +0530

    Update Program_6.java

--- Assignment3/Program_6.java
+++ Assignment3/Program_6.java
@@ -4,18 +4,8 @@ public class Program_6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Number of units consumed: ");
-        int unit = sc.nextInt();
-        double bill;
-
-        if (unit <= 50)
-            bill = unit * 3;
-        else if (unit <= 200)
-            bill = unit * 4.8;
-        else if (unit <= 400)
-            bill = unit * 5.8;
-        else
-            bill = unit * 6.2;
+        //
+        
 
         System.out.println("Amount payable: " + bill);
     }
