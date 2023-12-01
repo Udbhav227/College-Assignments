9447d66dc4d0510bee1b81f352dbfeb07a9838a9
Udbhav227 <143270660+Udbhav227@users.noreply.github.com>
2023-12-02 02:21:43 +0530

    Update HA_5.java

--- Assignment3/HA_5.java
+++ Assignment3/HA_5.java
@@ -4,35 +4,35 @@ public class HA_5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter gender (M/F): ");
+        System.out.print("What is your gender (M/F): ");
         String gender = sc.nextLine();
 
-        System.out.print("Enter first name: ");
+        System.out.print("First name: ");
         String firstName = sc.nextLine();
 
-        System.out.print("Enter last name: ");
+        System.out.print("Last name: ");
         String lastName = sc.nextLine();
 
-        System.out.print("Enter age: ");
+        System.out.print("Age: ");
         int age = sc.nextInt();
 
         if (gender.equalsIgnoreCase("F")) {
             if (age >= 20) {
-                System.out.print("Are you married? (y/n): ");
+                System.out.print("Are you married, " + firstName + " (y/n)? ");
                 sc.nextLine();
                 String martialStatus = sc.nextLine();
                 if (martialStatus.equalsIgnoreCase("y")) {
-                    System.out.println("Mrs. " + firstName + " " + lastName);
+                    System.out.println("Then I shall call you Mrs. " + firstName + " " + lastName);
                 } else {
-                    System.out.println("Ms. " + firstName + " " + lastName);
+                    System.out.println("Then I shall call you Ms. " + firstName + " " + lastName);
                 }
             } else {
                 System.out.println(firstName + " " + lastName);
             }
         } else if (gender.equalsIgnoreCase("M") && age >= 20) {
-            System.out.println("Mr. " + firstName + " " + lastName);
+            System.out.println("Then I shall call you Mr. " + firstName + " " + lastName);
         } else {
-            System.out.println(firstName + " " + lastName);
+            System.out.println("Then I shall call you " + firstName + " " + lastName);
         }
 
         sc.close();
