73eb577e3c004a91ea2d3b97195b192fdf782e70
Udbhav227 <143270660+Udbhav227@users.noreply.github.com>
2023-12-02 00:27:18 +0530

    Update Program_5.java

--- Assignment3/Program_5.java
+++ Assignment3/Program_5.java
@@ -7,10 +7,8 @@ public class Program_5 {
         System.out.print("Enter the year: ");
         int year = sc.nextInt();
 
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-            System.out.println(year + " is a leap year");
-        else
-            System.out.println(year + " is not a leap year");
+        boolean isLeapYear = year % 4 == 0 && ((year % 100 != 0) || (year % 400 == 0));
+        System.out.println(year + " is a leap year: " + isLeapYear);
 
         sc.close();
     }
