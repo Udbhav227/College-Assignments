52522f4efad07b415fd3ede1c232e932264c973f
Udbhav227 <143270660+Udbhav227@users.noreply.github.com>
2023-12-08 19:51:37 +0530

    Update Program_8.java

--- Assignment4/Program_8.java
+++ Assignment4/Program_8.java
@@ -5,20 +5,22 @@ public class Program_8 {
         Scanner sc = new Scanner(System.in);
 
         int compGuess = (int) (9 * Math.random() + 1);
-        int userGuess = 0;
+        int userGuess;
+        boolean isCorrect = false;
 
-        while(userGuess != compGuess) {
-            System.out.print("Your guess: ");
+        do {
+            System.out.print("Guess the number: ");
             userGuess = sc.nextInt();
 
-            if (userGuess == compGuess) {
-                System.out.println("Congrats! You got it right!");
-            } else if (userGuess > compGuess) {
-                System.out.println("Too high, try again!\n");
-            } else {
+            if (userGuess < compGuess)
                 System.out.println("Too low, try again!\n");
-            }
-        }
+            else if (userGuess > compGuess)
+                System.out.println("Too high, try again!\n");
+            else
+                isCorrect = true;
+        } while (!isCorrect);
+
+        System.out.println("Congrats! You got it right!");
 
         sc.close();
     }
