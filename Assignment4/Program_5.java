c06a2f059645129dec2df4df880e98c2a7678be9
Udbhav227 <143270660+Udbhav227@users.noreply.github.com>
2023-12-08 19:41:38 +0530

    Add files via upload

--- Assignment4/Program_5.java
+++ Assignment4/Program_5.java
@@ -7,15 +7,15 @@ public class Program_5 {
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
 
-        int sumOfFactors = 0;
+        int sum = 0;
 
-        for (int i = 2; i <= 28; i++) {
+        for (int i = 1; i < n; i++) {
             if (n % i == 0) {
-                sumOfFactors += n / i;
+                sum += i;
             }
         }
 
-        if (n == sumOfFactors)
+        if (n == sum)
             System.out.println(n + " is a perfect number.");
         else
             System.out.println(n + " is not a perfect number.");
