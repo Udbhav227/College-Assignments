846fd7c52a7ae804b5f04547f0f792b718ea828d
Udbhav227 <143270660+Udbhav227@users.noreply.github.com>
2023-12-12 03:04:28 +0530

    Update HA_1.java

--- Assignment4/HA_1.java
+++ Assignment4/HA_1.java
@@ -1,7 +1,6 @@
 public class HA_1 {
     public static void main(String[] args) {
         int sumOfSquare = 0, squareOfSum = 0;
-        
         for (int i = 1; i <= 10; i++) {
             sumOfSquare += i * i;
             squareOfSum += i;
