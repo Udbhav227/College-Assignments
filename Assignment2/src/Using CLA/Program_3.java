470d0d2ee91445d0117a33fc291a10fe8488b340
Koushik Das <koushikdasmagnate2005@gmail.com>
2024-01-21 16:12:27 +0530

    Update Program_3.java
    Code added .
    
    1) Taken input of two random integers using Random method of Math class in JAVA.
    2) To know the random integers generated by the method , printing both of them to make sure.
    3) Added comments for enhancing the understanding of the code.
    4) As Math.random() method generates double value , so type casted into integer .

--- Assignment2/src/Using CLA/Program_3.java
+++ Assignment2/src/Using CLA/Program_3.java
@@ -1,5 +1,18 @@
 public class Program_3 {
     public static void main(String[] args) {
-        // soon
+
+        // Input of two random integers  using random method of Math class. 
+        int  a =(int)( 1 +  (6-1+1)* Math.random());
+        int  b =(int)( 1 +  (6-1+1)* Math.random());
+
+        // To check the values of both the random integers generated.
+        System.out.println("The two random integers are : ");
+        System.out.println(a);
+        System.out.println(b);
+
+        // Finding sum of both the random integers.
+        System.out.println("Sum of the two random integers are: ");
+        System.out.println(a+b);
+
     }
 }
