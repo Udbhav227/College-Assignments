ac514e79eefcbe6794aeba8051ee032c23550e26
Udbhav Ashish Arya <143270660+Udbhav227@users.noreply.github.com>
2024-04-18 00:05:41 +0530

    Update CWQ3.java

--- Sem 2/ClassWork/CWQ3.java
+++ Sem 2/ClassWork/CWQ3.java
@@ -27,12 +27,12 @@ class Manager extends Employee implements EmpInterface {
 		this.bonus = bonus;
 	}
 	@Override
-	public void displayEmp() {
+	void displayEmp() {
 		// TODO Auto-generated method stub
 		
 	}
 	@Override
-	public void giveBonus(double amount) {
+	void giveBonus(double amount) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -64,35 +64,5 @@ abstract class Employee{
 		this.fName = fName;
 		this.lName = lName;
 		this.salary = salary;
-	}
-}
-
-class Manager extends Employee implements EmpInterface {
-	double bonus;
-	public Manager(int empID, String fName, String lName, double salary, double bonus) {
-		super(empID, fName, lName, salary);
-		if (bonus <= 0) {
-			throw new IllegalArgumentException("Bonus should be greater than 0");
-		}
-		this.bonus = bonus;
-	}
-	@Override
-	public void displayEmp() {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void giveBonus(double amount) {
-		// TODO Auto-generated method stub
-		
-	}
-}
-
-class Test {
-	public static void main(String[] args) {
-		EmpInterface[] arr = new EmpInterface[3];
-		
-		arr[0] = new Manager(420, "Walter ", "White ", 68000, 1000);
-		// And so on..
 	}
 }
