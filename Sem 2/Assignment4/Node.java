0f4e52328846713cf5de2ed15cf502fd3a21eb64
Udbhav Ashish Arya <143270660+Udbhav227@users.noreply.github.com>
2024-05-15 19:17:21 +0530

    Add files via upload

---
+++ Sem 2/Assignment4/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    protected int regd_no;
+    protected float mark;
+    protected Node next;
+
+    Node(int regd_no, float mark) {
+        this.regd_no = regd_no;
+        this.mark = mark;
+        next = null;
+    }
+}
