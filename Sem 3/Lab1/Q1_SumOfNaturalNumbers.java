cd7f74a196ec5e9c832c6a5d0f917347853b17cb
Udbhav Ashish Arya <143270660+Udbhav227@users.noreply.github.com>
2024-09-18 12:30:37 +0530

    Create Q1_SumOfNaturalNumbers.java

---
+++ Sem 3/Lab1/Q1_SumOfNaturalNumbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter n: ");
+      int n = sc.nextInt();
+      int sum = (n * (n + 1)) / 2;
+      System.out.print("Sum for first " + n + " natural number is: " + sum);
+  }
+}
+
+/**
+  Input: 5
+  Output: 15
+  Time Complexity: O(1)
+**/
