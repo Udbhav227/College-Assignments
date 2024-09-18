2112968cdf4c64713ef1e45963c8a6650a8b4ac5
Udbhav Ashish Arya <143270660+Udbhav227@users.noreply.github.com>
2024-09-18 12:52:10 +0530

    Update Q1_SumOfNaturalNumbers.java

--- Sem 3/Lab1/Q1_SumOfNaturalNumbers.java
+++ Sem 3/Lab1/Q1_SumOfNaturalNumbers.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class Q1 {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       System.out.print("Enter n: ");
